35d55e4b3c5b2eb4ef39a8521c922b3bc5511fcb
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-09-14 23:10:37 +0600

    countdown removing bug fixed

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
